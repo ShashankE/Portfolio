2440caf521bf97a968d1fe360aace26f712d524b
ShashankE <91713765+ShashankE@users.noreply.github.com>
2021-10-01 22:28:01 -0500

    Update FISD_Weighted_GPA_Calc.py

--- FISD_Weighted_GPA_Calc.py
+++ FISD_Weighted_GPA_Calc.py
@@ -1,3 +1,8 @@
+# This code takes 8 inputs of grades alongside with there course level inorder to calculate a FriscoISD Student's GPA
+# Shashank Ekkati 
+# Date Of Construction ~ 10/01/2021
+
+
 grade_list = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70]
 ap_gpa_correlation = [6.0, 5.9, 5.8, 5.7, 5.6, 5.5, 5.4, 5.3, 5.2, 5.1, 5.0, 4.9, 4.8, 4.7, 4.6, 4.5, 4.4, 4.3, 4.2, 4.1, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3.0]
 adv_gpa_correlation = [5.5, 5.4, 5.3, 5.2, 5.1, 5.0, 4.9, 4.8, 4.7, 4.6, 4.5, 4.4, 4.3, 4.2, 4.1, 4.0, 3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3.0, 2.9, 2.8, 2.7, 2.6, 2.5]
